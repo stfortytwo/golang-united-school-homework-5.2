3f10ff4f03ae4be7f2e8020a3bf0c8902fa2688a
st.forty.two <st.forty.two@gmail.com>
2022-07-09 12:34:17 +0300

    simple version without expiration

--- cache.go
+++ cache.go
@@ -1,23 +1,39 @@
 package cache
 
-import "time"
-
 type Cache struct {
+	items map[string]Element
+}
+
+type Element struct {
+	value string
 }
 
 func NewCache() Cache {
-	return Cache{}
+	items_map := make(map[string]Element)
+	c := Cache{
+		items: items_map,
+	}
+	return c
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	el, inCache := c.items[key]
+	return el.value, inCache
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.items[key] = Element{
+		value: value,
+	}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	outputKeys := make([]string, 0, len(c.items))
+	for key := range c.items {
+		outputKeys = append(outputKeys, key)
+	}
+	return outputKeys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
-}
+//func (receiver) PutTill(key, value string, deadline time.Time) {
+//}
